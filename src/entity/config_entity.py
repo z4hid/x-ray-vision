4672d034b96a520d14e205d0bb3bc29569267fb6
z4hid <zahidhasan9297@gmail.com>
2024-09-29 03:45:48 +0600

    config entity changes

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -1,3 +1,7 @@
 from dataclasses import dataclass
 from src.utils.main_utils import read_yaml_file
 
+@dataclass
+class DataIngestionConfig:
+    def __init__(self):
+        self.data_ingestion_config = read_yaml_file(CONFIG_PATH)
