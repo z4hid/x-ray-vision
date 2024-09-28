be564ee3aef330c2a2c210b58211bf189edc6fcc
z4hid <zahidhasan9297@gmail.com>
2024-09-29 03:47:39 +0600

    constants added

---
+++ src/constants/__init__.py
@@ -0,0 +1,7 @@
+import os
+import torch
+from datetime import datetime
+
+# Common Constants
+CONFIG_PATH: str = os.path.join(os.getcwd(), "config", "config.yaml")
+DATA_PATH: str = os.path.join(os.getcwd(), "data")
