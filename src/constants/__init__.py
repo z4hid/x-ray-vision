5deaf547b5b1f22cb8bdd641a6da62e7e1b7033b
z4hid <zahidhasan9297@gmail.com>
2024-09-29 03:49:45 +0600

    common constants added

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -4,4 +4,8 @@ from datetime import datetime
 
 # Common Constants
 CONFIG_PATH: str = os.path.join(os.getcwd(), "config", "config.yaml")
+TIMESTAMP: str = datetime.now().strftime("m%_d_%Y_%H_%M_%S")
 DATA_PATH: str = os.path.join(os.getcwd(), "data")
+ARTIFACTS_DIR: str = os.path.join("artifacts", TIMESTAMP)
+use_cuda = torch.cuda.is_available()
+DEVICE = torch.device("cuda" if use_cuda else "cpu")
