87f0967b5ea5bd5593e77ba56f9d4e0851c3ded9
z4hid <zahidhasan9297@gmail.com>
2024-09-30 01:50:43 +0600

    Data Transformation added on Training Pipeline

--- src/pipeline/training.py
+++ src/pipeline/training.py
@@ -3,13 +3,15 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_ingestion import DataIngestion
-from src.entity.config_entity import DataIngestionConfig
-from src.entity.artifact_entity import DataIngestionArtifacts
+from src.components.data_transformation import DataTransformation
+from src.entity.config_entity import DataIngestionConfig, DataTransformationConfig
+from src.entity.artifact_entity import DataIngestionArtifacts, DataTransformationArtifacts
 
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
 
     def start_data_ingestion(self) -> DataIngestionArtifacts:
@@ -25,9 +27,27 @@ class TrainingPipeline:
             raise CustomException(e, sys)
         
     
+    def start_data_transformation(self, data_ingestion_artifacts: DataIngestionArtifacts) -> DataTransformationArtifacts:
+        logging.info("Entered the start_data_transformation method of training pipeline class")
+        try:
+            data_transformation = DataTransformation(
+                data_ingestion_artifacts=data_ingestion_artifacts,
+                data_transformation_config=self.data_transformation_config
+            )
+            data_transformation_artifacts = (data_transformation.initiate_data_transformation())
+            logging.info("Exited the start_data_transformation method of training pipeline class")
+            return data_transformation_artifacts
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    
     def run_pipeline(self):
         try:
             logging.info("Entered the run_pipeline method of training pipeline class")
             data_ingestion_artifact = self.start_data_ingestion()
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifacts=data_ingestion_artifact
+                )
+            
         except Exception as e:
             raise CustomException(e, sys)
